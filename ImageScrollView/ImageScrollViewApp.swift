b9f1e83ad908b3983bf2a02f7c31cfe4aace2003
곽현우 <polla723@naver.com>
2024-07-26 17:02:43 +0900

    Initial Commit

---
+++ ImageScrollView/ImageScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageScrollViewApp.swift
+//  ImageScrollView
+//
+//  Created by 곽현우 on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
