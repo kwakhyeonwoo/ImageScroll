8e05fecff8a1c94adb79c0479cc04a1f50c723df
곽현우 <polla723@naver.com>
2024-07-28 13:23:51 +0900

    scrollView 생성하기

--- ImageScrollView/ContentView.swift
+++ ImageScrollView/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State fileprivate var click : String = ""
+    @State fileprivate var changeImage : Bool = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView{
+            
+            VStack{
+                Spacer()
+                HStack(alignment: .top, spacing: 0){
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("서울")
+                    })
+                    .frame(maxWidth: .infinity, maxHeight: 30)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("프랑스")
+                    })
+                    .frame(maxWidth: .infinity, maxHeight: 30)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("이탈리아")
+                    })
+                    .frame(maxWidth: .infinity, maxHeight: 30)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    
+                }
+                    
+                ZStack{
+                    Image("Image")
+                        .resizable()
+                    
+                    Spacer()
+                        .frame(maxHeight: .infinity)
+                }
+            }
+            
+            
+            LazyVStack{
+                if(click == "서울"){
+                    
+                    ScrollView(.vertical){
+                        
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
